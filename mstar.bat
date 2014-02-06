0b89536e8ba4fb2a372e897a4d8e19381e475e6a
enekogb <perreko@gmail.com>
2014-02-06 11:40:34 +0100

    Creado mstar.bat
    Creado este fichero como ejemplo de ejecucion de mstar.sh en entornos Windows con Cygwin

---
+++ mstar.bat
@@ -0,0 +1,10 @@
+@REM Script para ejecutar mstar.sh desde entornos Windows utilizando Cygwin (http://www.cygwin.com/)
+
+@REM Antes de ejecutar, es necesario tener Cygwin instalado y modificar la ruta de instalacion en la siguiente linea
+@REM Tambien es necesario modificar en la ultima linea el valor 123456 por el ID de portfolio de MStar.
+
+@set CYGWIN_BIN="c:\cygwin64\bin"
+@set PATH=%CYGWIN_BIN%;%PATH%
+
+%CYGWIN_BIN%\bash mstar.sh 123456
+@pause
