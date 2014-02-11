159f983bd3d08cefd572489863ac405daaf37fb8
enekogb <perreko@gmail.com>
2014-02-11 16:35:04 +0100

    Delete ftimes.bat

--- ftimes/ftimes.bat
+++
@@ -1,9 +0,0 @@
-@REM Script para ejecutar mstar.sh desde entornos Windows utilizando Cygwin (http://www.cygwin.com/)
-
-@REM Antes de ejecutar, es necesario tener Cygwin instalado y modificar la ruta de instalacion en la siguiente linea
-
-@set CYGWIN_BIN="c:\cygwin64\bin"
-@set PATH=%CYGWIN_BIN%;%PATH%
-
-%CYGWIN_BIN%\bash ftimes.sh
-@pause
