a5e9a65a71c1eaab8e697217db87197b35a49cd7
enekogb <perreko@gmail.com>
2014-02-17 11:27:36 +0100

    Update mstar.bat
    Cambiado para que use setEnv.bat

--- windows/mstar.bat
+++ windows/mstar.bat
@@ -1,10 +1,5 @@
 @REM Script para ejecutar mstar.sh desde entornos Windows utilizando Cygwin (http://www.cygwin.com/)
+call "setEnv.bat"
 
-@REM Antes de ejecutar, es necesario tener Cygwin instalado y modificar la ruta de instalacion en la siguiente linea
-@REM Tambien es necesario modificar en la ultima linea el valor 123456 por el ID de portfolio de MStar.
-
-@set CYGWIN_BIN="c:\cygwin64\bin"
-@set PATH=%CYGWIN_BIN%;%PATH%
-
-%CYGWIN_BIN%\bash mstar.sh -i -o salida 123456
+%CYGWIN_BIN%\bash %MSTAR%\mstar.sh -i -c %MSTAR%\salida\cookies.txt -o %MSTAR%\salida 123456
 @pause
