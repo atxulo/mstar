712d9e3e2107c5babcff0bdedf40af719ac9f439
enekogb <perreko@gmail.com>
2014-02-17 11:32:38 +0100

    Update mstar_euro_dolar.bat

--- windows/util/mstar_euro_dolar.bat
+++ windows/util/mstar_euro_dolar.bat
@@ -1,5 +1,5 @@
 @REM Script para ejecutar mstar_euro_dolar.sh desde entornos Windows utilizando Cygwin (http://www.cygwin.com/)
 call "../setEnv.bat"
 
-%CYGWIN_BIN%\bash %MSTAR%\util\mstar_euro_dolar.sh -o %MSTAR%\salida
+%CYGWIN_BIN%\bash %MSTAR%\util\mstar_euro_dolar.sh -o %MSTAR_SALIDA%
 @pause
