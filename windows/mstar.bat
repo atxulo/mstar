f2305a68dde8cfbe0ba11072ab6110c64dfdb306
enekogb <perreko@gmail.com>
2014-02-19 23:24:46 +0100

    Eliminado parametro -i

--- windows/mstar.bat
+++ windows/mstar.bat
@@ -1,5 +1,5 @@
 @REM Script para ejecutar mstar.sh desde entornos Windows utilizando Cygwin (http://www.cygwin.com/)
 call "setEnv.bat"
 
-%CYGWIN_BIN%\bash %MSTAR%\mstar.sh -i -c %MSTAR%\cookies.txt -o %MSTAR_SALIDA% 123456
+%CYGWIN_BIN%\bash %MSTAR%\mstar.sh -c %MSTAR%\cookies.txt -o %MSTAR_SALIDA% 123456
 @pause
