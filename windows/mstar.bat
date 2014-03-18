bcc92dfc1c0f7360150b85887b7ed268a88c11fb
enekogb <perreko@gmail.com>
2014-03-18 12:17:55 +0100

    Update mstar.bat
    Añadido -u y -p

--- windows/mstar.bat
+++ windows/mstar.bat
@@ -1,5 +1,5 @@
 @REM Script para ejecutar mstar.sh desde entornos Windows utilizando Cygwin (http://www.cygwin.com/)
 call "setEnv.bat"
 
-%CYGWIN_BIN%\bash %MSTAR%\mstar.sh -b %MSTAR_BACKUP% -c %MSTAR%\cookies.txt -o %MSTAR_SALIDA% 123456
+%CYGWIN_BIN%\bash %MSTAR%\mstar.sh -b %MSTAR_BACKUP% -c %MSTAR%\cookies.txt -u usuario -p password -o %MSTAR_SALIDA% 123456
 @pause
