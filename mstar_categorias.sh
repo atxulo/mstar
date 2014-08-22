29cd139629d45e9aabc3b028f19c4aec239a90a9
enekogb <perreko@gmail.com>
2014-08-22 17:29:59 +0200

    Script para obtencion de valores de categorias

---
+++ mstar_categorias.sh
@@ -0,0 +1,150 @@
+#!/bin/bash
+#
+# Autor: Eneko Gonzalez
+#
+# Script para generar un fichero CSV a partir de la lista de categorias de la pagina Morningstar.es
+AHORA=$(date +"%Y%m%d_%H%M%S_%N")
+
+# Parametros que se leen de la linea de comandos con sus valores por defecto
+CARPETA_BACKUP=
+CARPETA_OUT="out"
+VERBOSE=false
+
+# Funcion para copiar un fichero en una ruta de backup
+# $1 ruta del fichero del que hacer backup
+backupFichero() {
+  nombreFichero="${1##*/}"
+  cp $1 $CARPETA_BACKUP"/"$AHORA"_"$nombreFichero".bak"
+  if [ ! $? -eq 0 ]; then
+    echo "Error al hacer una copia de seguridad del fichero '$1' en '$CARPETA_BACKUP/$AHORA_$nombreFichero.bak', abortando."
+    exit 1
+  fi
+}
+
+# Funcion para escribir los mensajes en modo verbose
+# $1 mensaje a escribir
+mensaje() {
+	if [[ "$VERBOSE" = true ]]; then echo $1 1>&2; fi
+}
+
+
+trim() {
+    local var=$@
+    var="${var#"${var%%[![:space:]]*}"}"   # remove leading whitespace characters
+    var="${var%"${var##*[![:space:]]}"}"   # remove trailing whitespace characters
+    echo -n "$var"
+}
+
+# Funcion para explicar como los parametros
+usage() {
+cat << EOF
+
+USO: $0 opciones
+
+OPCIONES:
+   -b [ruta]       Path de la carpeta donde dejar una copia de seguridad de los ficheros. Si no se indica, no se hace copia.
+   -h              Muestra este mensaje de ayuda y finaliza
+   -o [ruta]       Path de la carpeta donde dejar los ficheros resultado. Si no existe, la intenta crear
+   -v              Modo verbose para depurar los pasos ejecutados
+
+EJEMPLOS:
+  $0 -o salida
+	  Genera el fichero en la carpeta 'salida'
+EOF
+}
+
+# Leemos los parametros del script
+while getopts "b:ho:v" opt; do
+  case $opt in
+    b)
+      CARPETA_BACKUP=$OPTARG
+	  ;;
+    h)
+      usage
+      exit 1
+      ;;
+    o)
+      CARPETA_OUT=$OPTARG
+      ;;
+    v)
+      VERBOSE=true
+      ;;
+    \?)
+      echo "Opcion desconocida: -$OPTARG" >&2
+      usage
+      exit 1
+      ;;
+    :)
+      echo "La opcion -$OPTARG requiere un parametro" >&2
+      usage
+      exit 1
+      ;;
+  esac
+done
+
+# Si no existe la carpeta de salida, la intentamos crear
+if [[ ! -d "${CARPETA_OUT}" ]]; then
+  mensaje "La carpeta $CARPETA_OUT no existe"
+  mkdir "$CARPETA_OUT"
+  mensaje "Carpeta $CARPETA_OUT creada"
+fi
+
+# Si se ha indicado, intentamos crear la carpeta de backup
+if [ ! -z "$CARPETA_BACKUP" ]; then
+  if [[ ! -d "${CARPETA_BACKUP}" ]]; then
+    mkdir "$CARPETA_BACKUP"
+    if [ ! $? -eq 0 ]; then
+      echo "Error crear la carpeta de backup '$CARPETA_BACKUP', abortando."
+      exit 1
+    fi
+  fi
+fi
+
+# Creamos los ficheros dat si no existen, y si existen, hacemos backup si asi se ha indicado
+if [ ! -f $CARPETA_OUT/mstar_categorias.dat ]; then
+  > $CARPETA_OUT/mstar_categorias.dat
+  mensaje "Creado fichero $CARPETA_OUT/mstar_categorias.dat"
+else
+  if [ ! -z "$CARPETA_BACKUP" ]; then
+    backupFichero "$CARPETA_OUT/mstar_categorias.dat"
+  fi
+fi
+
+# Nos conectamos a la pagina para extraer los datos de las categorias
+# No se puede poner --remote-encoding=ISO-8859-1 porque da error en windows
+wget --output-document=$CARPETA_OUT/mstar_categorias.html.tmp "http://www.morningstar.es/es/tools/categoryoverview.aspx"
+if [ $? -ne 0 ]; then
+  echo "Error al descargar el contenido de http://www.morningstar.es/es/tools/categoryoverview.aspx, abortando"
+  exit $?
+fi
+
+# Leemos el fichero descargado y lo convertimos en un fichero con este formato: "ID;Nombre;YTD;Fecha"
+cat $CARPETA_OUT/mstar_categorias.html.tmp | grep CategoryOverviewTable | sed -e 's/<\/td>/\n/g' -e 's/<tbody>/<tbody>\n/g' | sed -ne '/gridCategoryName/p' -ne '/gridReturnM0/p' -ne '/gridTrailingDate/p' | sed -ne 's/.*<td[^>]*>//p' | sed -e 's/.*default\.aspx?category=\([^"]*\)" title="\([^"]*\)".*/\1\n\2/' | gawk 'ORS=NR%4?";":"\n"' | sed -e '/\;-$/d' > $CARPETA_OUT/mstar_categorias.dat.tmp 
+
+# Juntamos el fichero temporal con el dat, ordenando alfabeticamente (asc)  y por fecha (desc)
+cat $CARPETA_OUT/mstar_categorias.dat $CARPETA_OUT/mstar_categorias.dat.tmp | sort -u -t\; -k2,2r -k4,4 -o $CARPETA_OUT/mstar_categorias.dat
+
+# Si ya existe el CSV, hacemos el backup si asi se ha indicado 
+if [[ -f "${CARPETA_OUT}/mstar_categorias.csv" ]]; then
+  if [ ! -z "$CARPETA_BACKUP" ]; then
+    backupFichero "$CARPETA_OUT/mstar_categorias.csv"
+  fi
+fi
+
+# Copiamos el fichero dat como csv corrigiendo el encoding para no perder acentos
+# solo si esta instalado iconv
+command -v iconv >/dev/null 2>&1
+if [ $? -ne 0 ]; then
+  echo "iconv NO INSTALADO. Es posible que se pierdan los acentos y caracteres especiales"
+  cp -f $CARPETA_OUT/mstar_categorias.dat $CARPETA_OUT/mstar_categorias.csv 
+else
+  iconv -f utf-8 -t iso-8859-1 <  $CARPETA_OUT/mstar_categorias.dat > $CARPETA_OUT/mstar_categorias.csv
+fi
+
+
+# Borramos los ficheros temporales generados
+rm $CARPETA_OUT/mstar_categorias.html.tmp
+rm $CARPETA_OUT/mstar_categorias.dat.tmp
+
+mensaje "Proceso finalizado correctamente"
+exit 0
